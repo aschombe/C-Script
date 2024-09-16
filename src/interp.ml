75e17d410129431d238bdb7e0fe161a72e22ce3c
andrew <aschombe@stevens.edu>
2024-09-16 12:40:29 -0400

    Changed scoping method, updated interp to reflect scoping

--- src/interp.ml
+++ src/interp.ml
@@ -1,45 +1,42 @@
 open Ast
 open Read_file
+open Scope
 
 exception RunTimeError of string
 
-let var_env = Hashtbl.create 10
-let func_env = Hashtbl.create 10
+let env = create_scope ()
 
 let rec interp_ast = function
   | Int n -> n
-  | Float f -> f
-  | Bool b -> b
-  | Var v -> (try Hashtbl.find var_env v with Not_found -> raise (RunTimeError ("variable not found: " ^ v)))
+  | Float f -> int_of_float f
+  | Bool b -> if b then 1 else 0
+  | Var v -> (try get_var v with Not_found -> raise (RunTimeError ("variable not found: " ^ v)))
   | BinOp (op, e1, e2) -> (
-    let v1 = interp_ast e1 in
-    let v2 = interp_ast e2 in
     match op with
     | Add -> v1 + v2
     | Sub -> v1 - v2
     | Mul -> v1 * v2
     | Div -> if v2 = 0 then raise (RunTimeError "division by zero") else v1 / v2
-    | Mod -> v1 mod v2
+    | Mod -> if v2 = 0 then raise (RunTimeError "division by zero") else v1 mod v2
     | Pow -> int_of_float (float_of_int v1 ** float_of_int v2)
     | Eq -> if v1 = v2 then 1 else 0
     | Neq -> if v1 <> v2 then 1 else 0
     | Lt -> if v1 < v2 then 1 else 0
     | Gt -> if v1 > v2 then 1 else 0
-    | Le -> if v1 <= v2 then 1 else 0
-    | Ge -> if v1 >= v2 then 1 else 0
+    | Lte -> if v1 <= v2 then 1 else 0
+    | Gte -> if v1 >= v2 then 1 else 0
     | And -> if v1 <> 0 && v2 <> 0 then 1 else 0
     | Or -> if v1 <> 0 || v2 <> 0 then 1 else 0
-  )
+    )
   | UnOp (op, e) -> (
     let v = interp_ast e in
     match op with
-    (* | Neg -> -v *)
-    | Sub -> -v 
+    | Neg -> -v 
     | Not -> if v = 0 then 1 else 0
   )
   | Let (v, t, e) -> (
     let v' = interp_ast e in
-    Hashtbl.add var_env v v';
+    set_var v v';
     v'
   )
   | Set (v, op, e) -> (
@@ -54,24 +51,26 @@ let rec interp_ast = function
       | ModEq -> v mod v'
       | PowEq -> int_of_float (float_of_int v ** float_of_int v')
     in
-    Hashtbl.replace var_env v v'';
+    set_var v v'';
     v''
   )
   | Del v -> (
-    Hashtbl.remove var_env v;
+    remove_var v;
     0
   )
   | FuncDef (f, args, t, e) -> (
-    Hashtbl.add func_env f (args, t, e);
+    set_func f args e;
     0
   )
   | Call (f, args) -> (
-    let (args', t, e) = (try Hashtbl.find func_env f with Not_found -> raise (RunTimeError ("function not found: " ^ f))) in
-    let args'' = List.map2 (fun (a, _) e -> (a, interp_ast e)) args' args in
-    List.iter (fun (a, v) -> Hashtbl.add var_env a v) args'';
-    let v = interp_ast e in
-    List.iter (fun (a, _) -> Hashtbl.remove var_env a) args';
-    v
+    (* use get_func to get the function *)
+    let args' = List.map interp_ast args in
+    let (arg_names, body) = get_func f in
+    push_scope ();
+    List.iter2 set_var arg_names args';
+    let ret = interp_ast body in
+    pop_scope ();
+    ret
   )
   (* | IEE (cases, default) -> (
     let rec interp_ast_cases = function
--- src/scope.ml
+++ src/scope.ml
@@ -9,64 +9,61 @@ type value =
 | Function of string * (string * var_types) list * func_types * expr
 
 type scope = {
-  variables: (string, value) Hashtbl.t;
-  functions: (string, (string * var_types) list * func_types * expr) Hashtbl.t;
-  mutable cache: value option;
-  parent: scope option;
+  vars: (string, value) Hashtbl.t;
+  funcs: (string, (string * var_types) list * func_types * expr list) Hashtbl.t;
 }
 
-let create_scope parent_scope = {
-  variables = Hashtbl.create 10;
-  functions = Hashtbl.create 10;
-  cache = None;
-  parent = parent_scope;
+let create_scope () = {
+  vars = Hashtbl.create 10;
+  funcs = Hashtbl.create 10;
 }
 
-let scope_stack = Stack.create()
+let scope_stack = ref [ create_scope () ]
 
-let enter_scope () =
-  let parent_scope = if Stack.is_empty scope_stack then None else Some (Stack.top scope_stack) in
-  let new_scope = create_scope parent_scope in
-  Stack.push new_scope scope_stack
+let push_scope () =
+  scope_stack := create_scope () :: !scope_stack
 
-let exit_scope () =
-  if not (Stack.is_empty scope_stack) then
-    ignore (Stack.pop scope_stack)
+let pop_scope () =
+  match !scope_stack with
+  | _ :: tl -> scope_stack := tl
+  | [] -> failwith "pop_scope: empty scope stack"
 
-let lookup_variable name =
-  let rec lookup_var_in_scope scope =
-    try
-      if Hashtbl.mem scope.variables name then
-        Some (Hashtbl.find scope.variables name)
-      else if Hashtbl.mem scope.variables name then
-        let value = Hashtbl.find scope.variables name in
-        Hashtbl.add scope.variables name value;
-        Some value
-      else
-        match scope.parent with
-        | Some parent -> lookup_var_in_scope parent
-        | None -> None
-    with Not_found -> None
-  in
-  if Stack.is_empty scope_stack then None else lookup_var_in_scope (Stack.top scope_stack)
+let set_var name expr =
+  match !scope_stack with
+  | [] -> failwith "set_var: empty scope stack"
+  | env :: _ -> Hashtbl.replace env.vars name expr
 
-let lookup_function name =
-  let rec lookup_func_in_scope scope =
-    try
-      if Hashtbl.mem scope.functions name then
-        Some (Hashtbl.find scope.functions name)
-      else
-        match scope.parent with
-        | Some parent -> lookup_func_in_scope parent
-        | None -> None
-    with Not_found -> None
-  in
-  if Stack.is_empty scope_stack then None else lookup_func_in_scope (Stack.top scope_stack)
+let get_var name =
+  let rec lookup env_stack =
+    match env_stack with
+    | [] -> failwith ("get_var: variable not found: " ^ name)
+    | env :: rest ->
+        try Hashtbl.find env.vars name
+        with Not_found -> lookup rest
+  in 
+  lookup !scope_stack
 
-let add_variable name value =
-  if not (Stack.is_empty scope_stack) then
-    Hashtbl.add (Stack.top scope_stack).variables name value
+let remove_var name =
+  match !scope_stack with
+  | [] -> failwith "remove_var: empty scope stack"
+  | env :: _ -> Hashtbl.remove env.vars name
 
-let add_function name func =
-  if not (Stack.is_empty scope_stack) then
-    Hashtbl.add (Stack.top scope_stack).functions name func
+let set_func name args ret_type body =
+  match !scope_stack with
+  | [] -> failwith "set_func: empty scope stack"
+  | env :: _ -> Hashtbl.replace env.funcs name (args, ret_type, body)
+
+let get_func name =
+  let rec lookup env_stack =
+    match env_stack with
+    | [] -> failwith ("get_func: function not found: " ^ name)
+    | env :: rest ->
+        try Hashtbl.find env.funcs name
+        with Not_found -> lookup rest
+  in 
+  lookup !scope_stack
+
+let remove_func name =
+  match !scope_stack with
+  | [] -> failwith "remove_func: empty scope stack"
+  | env :: _ -> Hashtbl.remove env.funcs name
