4c42ff89720e13fe0d120a57728708a4082c4a3f
andrew <aschombe@stevens.edu>
2024-09-18 11:48:56 -0400

    Started working on lexer

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -25,6 +25,133 @@ Keywords and symbols in my language:
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <regex>
 
+/* takes a string of code and returns a vector of tokens */
+class Lexer {
+  public:
+    // constructor
+    Lexer(std::string code);
+    
+    // destructor
+    ~Lexer();
+    
+    // get the next token
+    std::string next_token();
+    
+    // peek the next token (don't consume it)
+    std::string peek_token();
+    
+    // get the current line
+    int get_line();
+
+    // get the current column
+    int get_column();
+
+    // get the current snippet
+    std::string get_snippet();
+
+    // get the current position
+    int get_pos();
+
+    // get the current token
+    std::string get_token();
+
+    // tokenize the code
+    std::vector<std::string> tokenize();
+
+  private:
+    int line;
+    int column;
+    
+    // snippet for tokenizer error messages
+    std::string snippet;
+    
+    // code to tokenize
+    std::string code;
+    
+    // current position in the code
+    int pos;
+    
+    // current token
+    std::string token;
+    
+    // regex for Keywords
+    std::map<std::string, std::string> keywords = {
+      {"let", "let"},
+      {"set", "set"},
+      {"del", "del"},
+      {"if", "if"},
+      {"elif", "elif"},
+      {"else", "else"},
+      {"for", "for"},
+      {"while", "while"},
+      {"break", "break"},
+      {"continue", "continue"},
+      {"return", "return"},
+      {"exit", "exit"},
+      {"func", "func"},
+      {"int", "int"},
+      {"float", "float"},
+      {"bool", "bool"},
+      {"string", "string"},
+      {"switch", "switch"},
+      {"case", "case"},
+      {"default", "default"},
+      {"true", "true"},
+      {"false", "false"}
+    };
+
+    // regex for operators and symbols
+    std::map<std::string, std::string> symbols = {
+      {"+", "+"},
+      {"-", "-"},
+      {"*", "*"},
+      {"/", "/"},
+      {"%", "%"},
+      {"^", "^"},
+      {"==", "=="},
+      {"!=", "!="},
+      {"<", "<"},
+      {"<=", "<="},
+      {">", ">"},
+      {">=", ">="},
+      {"&&", "&&"},
+      {"||", "||"},
+      {"=", "="},
+      {"+=", "+="},
+      {"-=", "-="},
+      {"*=", "*="},
+      {"/=", "/="},
+      {"%=", "%="},
+      {"^=", "^="},
+      {"(", ")"},
+      {"{", "}"},
+      {"[", "]"},
+      {",", ","},
+      {":", ":"},
+      {";", ";"}
+    };
+
+    // regex for comments (same as c++, //, /*, */)
+    std::regex comment_regex{"(//|/\\*|\\*/)"};
+
+    // regex for identifiers
+    std::regex identifier_regex{"[a-zA-Z_][a-zA-Z0-9_]*"};
+
+    // regex for literals
+    std::regex int_regex{"[0-9]+"};
+    std::regex float_regex{"[0-9]+\\.[0-9]+"};
+    std::regex string_regex{"\"[^\"]*\""};
+    std::regex bool_regex{"(true|false)"};
+
+    // regex for whitespace (' ', '\t', '\n')
+    std::regex whitespace_regex{"[ \t\n]"};
+
+};
 
 #endif // LEXER_HPP
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -21,3 +21,75 @@ Keywords and symbols in my language:
     8: Logical OR (||)
     9 (lowest): assignment (=, +=, -=, *=, /=, %=)
 */
+
+#include "../include/lexer.hpp"
+
+// constructor
+Lexer::Lexer(std::string code) {
+  this->code = code;
+  this->line = 1;
+  this->column = 1;
+  this->pos = 0;
+  this->snippet = "";
+  // keywords, symbols, comments, identifier, and literal regex is already defined in lexer.hpp
+  // so we dont have to add to the maps here
+}
+
+// destructor
+Lexer::~Lexer() {
+  // nothing to do here yet
+}
+
+// get current token
+std::string get_token() {
+  return this->token;
+}
+
+// get current line
+int get_line() {
+  return this->line;
+}
+
+// get current column
+int get_column() {
+  return this->column;
+}
+
+// get current position
+int get_pos() {
+  return this->pos;
+}
+
+// get current snippet
+std::string get_snippet() {
+  return this->snippet;
+}
+
+// get the next token
+std::string next_token() {
+  // if we are at the end of the code, return an empty string
+  if (this->pos >= this->code.size()) {
+    return "";
+  }
+  // if we are at the end of the line, increment the line and reset the column
+  if (this->code[this->pos] == '\n') {
+    this->line++;
+    this->column = 1;
+  }
+  // if we are at a space, tab, or newline, skip it (regex for it called whitespace_regex)
+  if 
+    this->pos++;
+    this->column++;
+    return next_token();
+  }
+}
+
+// peek the next token
+std::string peek_token() {
+
+}
+
+// tokenize the code
+std::vector<std::string> tokenize() {
+
+}
